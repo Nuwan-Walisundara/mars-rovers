f3bc10289558e7b942623d5ed11193fedc48e2e4
Nuwan Walisundara <nuwan@wso2telco.com>
2017-02-26 10:59:15 +0530

    initial project structure

---
+++ src/main/java/org/challenge/app/App.java
@@ -0,0 +1,13 @@
+package org.challenge.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
